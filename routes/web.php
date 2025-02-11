fdb57ac3ae867d77997bd563790bd91099c3f078
pablocoop <pabloosorionavarro@gmail..com>
2025-02-11 08:39:43 -0300

    ADD some new stuff

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+// http://localhost/laravel/blog/public/
 Route::get('/', function () {
     return "view('welcome')";
 });
+
+// una Route puede ser de tipo: get, post, put, patch, delete
