0c8ed83ac3ee28699bff18ea7b72265abe1605f0
pablocoop <pabloosorionavarro@gmail..com>
2025-02-11 13:26:27 -0300

    ADD new route to test changes and  ADD new model Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,5 +11,9 @@ class Post extends Model
 
     protected $table = 'posts';
 
+    // Esto último es opcional, pues de por sí trabajará con una convención:
+    // Si el model se llama Post, la tabla será posts (minúscula plural-inglés)
+    // EJ: Category -> categories
+
     //
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
+
 //Route::get('/', [HomeController::class, 'index']);
 Route::get('/', HomeController::class); //no lleva el método pues __invoke() es el único
 
@@ -11,6 +13,56 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 
 Route::get('/posts/{post}', [PostController::class, 'show']);
+
+Route::get('prueba', function(){
+    /*
+    Crear nuevo post
+    
+    $post = new Post;
+    $post->title = 'Título de prueba 3';
+    $post->content = 'Contenido de prueba 3';
+    $post->category = 'Categoría de prueba 3';
+
+    $post->save();
+
+    return $post;
+
+    */
+    
+
+
+    // $post = Post::find(1);
+
+    /*
+    $post = Post::where('title', 'Título de prueba 1')
+                ->first();
+    $post->category = "Desarrollo Web";
+    $post->save();
+    */
+
+    /*
+    $post = Post::all();
+    return $post;
+    */
+
+    /*
+    $posts = Post::where('id', '>=', '2')
+                        ->get();
+    return $posts;
+    */
+    /*
+    $posts = Post::orderBy('id', 'desc')
+                    ->get();
+    return $posts;
+    */
+    /* Listar todos los posts
+    $posts = Post::orderBy('id', 'desc')
+                    ->select('id', 'title', 'category')
+                    //->take(2) //para obtener solo 2 registros
+                    ->get();
+    return $posts;
+    */
+});
 
 
 
